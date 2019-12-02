2fe4fa9f4be07d3dde0f3911692df76562417bf0
joy <joy.lai@zg.com>
2019-12-02 22:56:46 +0800

    fix

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,11 +1,8 @@
 import '@pefish/js-node-assist'
 import path from 'path'
-import { AppUtil, ElectronRouteFactoryHelper } from '@pefish/js-electron-common'
+import { App } from '@pefish/js-electron-common'
 import FileUtil from '@pefish/js-util-file'
-import electron from 'electron'
-import { Log4js } from '@pefish/js-helper-logger'
 import ConfigUtil from '@pefish/js-util-config'
-import os from 'os'
 
 declare global {
   namespace NodeJS {
@@ -13,38 +10,24 @@ declare global {
       logger: any,
       config: {[x: string]: any};
       debug: boolean;
-      mainWindow: electron.BrowserWindow;
-      dataDir: string
+      app: App;
     }
   }
 }
 
-AppUtil.onReady(async () => {
-  global.logger.info(`启动中。。。`)
-  global.config = ConfigUtil.loadYamlConfig({
-    configFilePath: path.join(FileUtil.getStartFilePath(), `../config/prod.json`),
-  })
-  global.debug = global.config.env !== 'prod'
-  const packageInfo = require(path.join(FileUtil.getStartFilePath(), `../package.json`))
-  const homedir = os.homedir()
-  global.logger = new Log4js(packageInfo.name, `${homedir}/.${packageInfo.name}/log`)
-  global.dataDir = `${homedir}/.${packageInfo.name}/data`
-  FileUtil.mkdirSync(global.dataDir)
-
-  await new ElectronRouteFactoryHelper().buildRoute(
-    path.join(__dirname, './controller')
-  )
-
-  global.mainWindow = AppUtil.getMainWindow({
-    height: 600,
-    width: 800,
-  })
-  const file = path.join(FileUtil.getStartFilePath(), '../build/index.html')
-  let url = 'file://' + file
-  if (global.debug) {
-    url = `http://localhost:3000`
-  }
-  global.mainWindow.loadURL(url + `#/index`)
-  global.mainWindow.show()
-  global.logger.info(`启动成功!!!`)
+global.config = ConfigUtil.loadYamlConfig({
+  configEnvName: `NODE_CONFIG`,
+  configFilePath: path.join(FileUtil.getStartFilePath(), `../config/prod.yaml`),
 })
+global.debug = global.config.env !== 'prod'
+const packageInfo = require(path.join(FileUtil.getStartFilePath(), '../package.json'))
+
+global.app = new App(packageInfo)
+global.app.start(
+  async () => {
+    global.logger = global.app.logger
+  },
+  path.join(__dirname, './controller'),
+  path.join(FileUtil.getStartFilePath(), '../build/index.html'),
+  global.config,
+)
