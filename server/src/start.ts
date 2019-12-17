92303b0836ac6f011b17fcf9fd0dde26e0f0cdb5
joy <joy.lai@zg.com>
2019-12-17 14:59:05 +0800

    improve

--- server/src/start.ts
+++ server/src/start.ts
@@ -7,27 +7,24 @@ import ConfigUtil from '@pefish/js-util-config'
 declare global {
   namespace NodeJS {
     interface Global {
-      logger: any,
-      config: {[x: string]: any};
-      debug: boolean;
       app: App;
     }
   }
 }
 
-global.config = ConfigUtil.loadYamlConfig({
+const config = ConfigUtil.loadYamlConfig({
   configEnvName: `NODE_CONFIG`,
   configFilePath: path.join(FileUtil.getStartFilePath(), `../config/prod.yaml`),
 })
-global.debug = global.config.env !== 'prod'
-const packageInfo = require(path.join(FileUtil.getStartFilePath(), '../package.json'))
-
-global.app = new App(packageInfo)
+global.app = new App(
+  require(path.join(FileUtil.getStartFilePath(), '../package.json')),
+  config,
+  config.env !== 'prod'
+)
 global.app.start(
   async () => {
-    global.logger = global.app.logger
+    
   },
   path.join(__dirname, './controller'),
   path.join(FileUtil.getStartFilePath(), '../build/index.html'),
-  global.config,
 )
